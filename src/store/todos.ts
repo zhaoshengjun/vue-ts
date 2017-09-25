6c34ade19ba28e5e6c0875387feddab4c4bbf3dd
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-25 13:32:20 +1000

    use vuex to create a store

---
+++ src/store/todos.ts
@@ -0,0 +1,9 @@
+import { State } from "../types";
+
+const state: State = {
+  todos: [{ text: "Buy Milk" }]
+};
+
+export const todos = {
+  state
+};
---
+++ src/types.ts
@@ -0,0 +1,7 @@
+export interface Todo {
+  text: string;
+}
+
+export interface State {
+  todos: Todo[];
+}
