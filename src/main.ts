081ed2b4178d4159d59756af662a2589a59ca562
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-25 15:13:58 +1000

    dynamic import

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { store } from "./store";
+// import { login } from "./store/login";
 
 Vue.config.productionTip = false;
+
+const load = true;
+
+if (load) {
+  import("./store/login").then(({ login }) => {
+    store.registerModule("login", login);
+  });
+}
 
 new Vue({
   el: "#app",
