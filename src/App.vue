e68711f50f852b19883a258607bf47f8ac00397f
Shengjun Zhao <Shengjun.Zhao@dsiti.qld.gov.au>
2017-09-25 13:22:38 +1000

    init commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <ul>
+      <li v-for="todo in todos"> {{todo.text}} </li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import { Component } from "vue-property-decorator";
+
+  @Component
+  export default class App extends Vue {
+    todos = [{text: 'Buy milk'}]
+  }
+</script>
